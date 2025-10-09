726636e021b2bf62ad72f1504cc8cc7d4f6a0f50
hakim <hakimzack42@gmail.com>
2025-10-09 17:25:42 +0800

    edited the report on sale_port_rate - Hakim

--- server/odoo/custom_addons/hanson_letter_t3/__manifest__.py
+++ server/odoo/custom_addons/hanson_letter_t3/__manifest__.py
@@ -8,7 +8,7 @@
     "data": [
         'security/ir.model.access.csv',
         "views/sale_letter_quotation_view.xml",
-        "views/report_t3.xml",
+        # "views/report_t3.xml",
     ],
     "installable": True,
     "application": False,
